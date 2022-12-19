57e6fa176a6164717b3f4581ce7ef8a54d5f4835
ke.li <leecoor97@gmail.com>
2022-12-19 16:20:54 -0500

    update detail page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import MenuList from "./routes/MenuList";
 import MenuDetail from "./routes/MenuDetail";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -22,8 +20,3 @@ root.render(
 		</React.StrictMode>
 	</BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
